b1852b22126e0bae345e96ff835c371e7552ce8d
Priyam Anand <priyam27.anand@gmail.com>
2024-06-11 12:23:50 +0530

    fix: updated deployment params for etherlink

--- markets/etherlink/index.ts
+++ markets/etherlink/index.ts
@@ -1,7 +1,12 @@
 import { eEtherlinkNetwork, IAaveConfiguration } from "../../helpers/types";
 import { AaveMarket } from "../aave/index";
 import { strategyWETH } from "../aave/reservesConfigs";
-import { strategyWBTC, strategyEUSD, strategyUSDT } from "./reserveConfigs";
+import {
+  strategyWBTC,
+  strategyEUSD,
+  strategyUSDT,
+  strategySTXTZ,
+} from "./reserveConfigs";
 // ----------------
 // POOL--SPECIFIC PARAMS
 // ----------------
@@ -9,34 +14,31 @@ export const EtherlinkV3Market: IAaveConfiguration = {
   ...AaveMarket,
   ProviderId: 30,
   WrappedNativeTokenSymbol: "WXTZ",
-  MarketId: "Aave Etherlink Market",
+  MarketId: "Plend Etherlink Market",
   ATokenNamePrefix: "Etherlink",
   StableDebtTokenNamePrefix: "Etherlink",
   VariableDebtTokenNamePrefix: "Etherlink",
   SymbolPrefix: "Xtz",
   ReserveAssets: {
     [eEtherlinkNetwork.etherlinkTestnet]: {
-      WBTC: "0x7Cd53e97DDF8D2B370db53F5C3c753B1939208EC",
-      USDT: "0xF0Fab3d8d94B5225ee42CfdcE4c257399135202c",
-      EUSDC: "0xA9A5779cA171d0577eBC372bc5aa863E3bF1051d",
+      WSTXTZ: "0xAf9d5B2a60F22B85325738669e804B39d3a99583",
+      STXTZ: "0xaf1C7a33eb3994337bF71edF7d2C362b686E4ECD",
       WXTZ: "0xC2ef9495272B43F5257b35A1b6ddA78932839871",
     },
   },
   ChainlinkAggregator: {
     [eEtherlinkNetwork.etherlinkTestnet]: {
-      WBTC: "0xfe66A25096128f57D3876D42cD2B4347a77784c2",
-      USDT: "0xb87b753324248A737e8Dc69E1ba9F7f54e44aeB2",
-      EUSDC: "0xb87b753324248A737e8Dc69E1ba9F7f54e44aeB2",
-      WXTZ: "0xb87b753324248A737e8Dc69E1ba9F7f54e44aeB2",
+      WSTXTZ: "0x13fB1F126D71C4717c9f4815f51EfEeC4c037e3C", // oracle
+      STXTZ: "0x3A2bf7f2E1B41c3CF2Fb22975C737bEBE13226d1", // oracle
+      WXTZ: "0x3A2bf7f2E1B41c3CF2Fb22975C737bEBE13226d1",
     },
   },
   L2PoolEnabled: {
     [eEtherlinkNetwork.etherlinkTestnet]: false,
   },
   ReservesConfig: {
-    WBTC: strategyWBTC,
-    USDT: strategyUSDT,
-    EUSDC: strategyEUSD,
+    WSTXTZ: strategySTXTZ, // replace this with new strategy
+    STXTZ: strategySTXTZ, // replace this with new strategy
     WXTZ: strategyWETH,
   },
   EModes: {},
--- markets/etherlink/reserveConfigs.ts
+++ markets/etherlink/reserveConfigs.ts
@@ -2,7 +2,27 @@ import { eContractid, IReserveParams } from "../../helpers/types";
 import {
   rateStrategyStableOne,
   rateStrategyVolatileOne,
+  rateStrategyVolatileTwo,
 } from "../aave/rateStrategies";
+
+export const strategySTXTZ: IReserveParams = {
+  strategy: rateStrategyVolatileTwo,
+  baseLTVAsCollateral: "8000",
+  liquidationThreshold: "8250",
+  liquidationBonus: "10500",
+  liquidationProtocolFee: "1000",
+  borrowingEnabled: true,
+  stableBorrowRateEnabled: false,
+  flashLoanEnabled: true,
+  reserveDecimals: "18",
+  aTokenImpl: eContractid.AToken,
+  reserveFactor: "1000",
+  supplyCap: "0",
+  borrowCap: "0",
+  debtCeiling: "0",
+  borrowableIsolation: false,
+};
+
 export const strategyEUSD: IReserveParams = {
   strategy: rateStrategyStableOne,
   baseLTVAsCollateral: "8000",
